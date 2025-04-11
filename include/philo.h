c8c9bc5981ae7547b812ddc5cab8cd1e6be47f3d
WaPoco <110930971+WaPoco@users.noreply.github.com>
2025-04-11 19:11:41 +0200

    Added init_philo()

--- include/philo.h
+++ include/philo.h
@@ -6,7 +6,7 @@
 /*   By: vpogorel <vpogorel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 12:41:23 by vpogorel          #+#    #+#             */
-/*   Updated: 2025/04/05 15:49:01 by vpogorel         ###   ########.fr       */
+/*   Updated: 2025/04/11 15:16:35 by vpogorel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,22 @@
 
 typedef struct rules
 {
+	int	number_of_philosophers;
 	int	time_to_die;
 	int	time_to_eat;
 	int	time_to_sleep;
+	int	number_of_times_each_philosopher_must_eat;
 } t_rules;
 
 typedef struct philosopher 
 {
-    int	fork;
+	int	id;
+    int	lfork;
+	int	rfork;
+	int eat;
+	int wait;
 	struct timeval start;
 	struct timeval end;
 	pthread_t philo;
+	t_rules	*rules;
 } t_philosopher;
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: vpogorel <vpogorel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 12:00:36 by vpogorel          #+#    #+#             */
-/*   Updated: 2025/04/05 16:09:57 by vpogorel         ###   ########.fr       */
+/*   Updated: 2025/04/11 19:09:58 by vpogorel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,80 @@ void *thread_function(void *arg)
     }
     return (NULL);
 }
+int check_number(char *arg)
+{
+    int i;
+
+    i = 0;
+    if (arg == NULL)
+        return (0);
+    while (arg[i])
+    {
+        if (arg[i] > '9' || arg[i] < '0')
+            return (0);
+        i++;
+    }
+    return (1);
+}
+
+t_rules *read_input(int arg0, char **args)
+{
+    int i;
+    int *time;
+    t_rules *rules;
+
+    time = malloc((arg0 - 1) * sizeof(int));
+    rules = malloc(sizeof(t_rules));
+    if (!rules || !time)
+        return (NULL);
+    i = 0;
+    while (++i < arg0)
+    {
+        if (check_number(args[i]) == 1)
+            time[i] = atoi(args[i]);
+        else
+            return (free(time), free(rules), NULL);
+    }
+    rules->number_of_philosophers = time[0];
+    rules->time_to_die = time[1];
+    rules->time_to_eat = time[2];
+    rules->time_to_sleep = time[3];
+    rules->number_of_times_each_philosopher_must_eat = time[4];
+    free(time);
+    return (rules);
+}
+
+t_philosopher   *init_philo(int arg0, char **args)
+{
+    t_philosopher   *philos;
+    t_rules         *rules;
+    int             *input_philo;
+    int             i;
+
+    i = 0;
+    rules = malloc(sizeof(t_rules));
+    if (!rules)
+        return (philos);
+    rules = read_input(arg0, args);
+    philos = malloc(sizeof(t_philosopher) * rules->number_of_philosophers);
+    if (!philos)
+        return (philos);
+    while (i < rules->number_of_philosophers)
+    {
+        philos->id = i;
+        philos->rules = rules;
+        i++;
+    }
+    return (philos);
+}
 
 int main(int arg0, char **args)
 {
+    t_philosopher   *philos;
+
+    philos = init_philo(arg0, args);
+    printf("Hello, I'am philosopher number %d \n", philos[2].id);
+    /*
     t_philosopher *p1;
     t_philosopher *p2;
 
@@ -44,14 +115,15 @@ int main(int arg0, char **args)
     pthread_create(&p1->philo, NULL, thread_function, (void *)p1);
     pthread_create(&p2->philo, NULL, thread_function, (void *)p2);
     pthread_join(p1->philo, NULL);
-    sleep(1);
+    sleep(3);
     gettimeofday(&p1->end, NULL);
     pthread_join(p2->philo, NULL);
     sleep(3);
     gettimeofday(&p2->end, NULL);
     printf("mails: %d\n", mails);
     printf("Time of Philo 1 %ld\n", p1->end.tv_sec - p1->start.tv_sec);
-    printf("Time of Philo 2 %ld", p2->end.tv_sec - p2->start.tv_sec);
+    printf("Time of Philo 2 %ld\n", p2->end.tv_sec - p2->start.tv_sec);
     pthread_mutex_destroy(&mutex);
+    */
     return (0);
 }
